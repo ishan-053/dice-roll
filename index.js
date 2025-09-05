da9ff1d2cb8685d9a939629232e7617ce159a0db
ishan-053 <shindeishan042@gmail.com>
2025-09-05 21:34:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomnumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource1 = "images/dice" + randomnumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+var randomnumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomnumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomnumber1 > randomnumber2) {
+  document.querySelector("h1").innerHTML = " 🚩 Player 1 Wins! ";
+} else if (randomnumber2 > randomnumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw !!!";
+}
